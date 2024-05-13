c31f944870cfa007302ce813423fc59e1c133f0c
IWEYRTX <iweyrtx@gmail.com>
2024-05-13 19:30:17 +0000

    1

---
+++ assest/Photoassest/js/wexium.js
@@ -0,0 +1,5 @@
+const heroSection = document.getElementById('hero');
+const animateBtn = document.getElementById('animate-btn');
+
+animateBtn.addEventListener('click', () => {
+  heroSection.classList
